c369ddc3f9afa533397d8b923b7b4fcf45d13060
Steviegt6 <xxlennygamerxx@gmail.com>
2022-05-01 19:23:38 -0700

    Base TeaMod implementation.

---
+++ ITeaMod.cs
@@ -0,0 +1,15 @@
+﻿using Terraria.ModLoader;
+
+namespace TeaFramework
+{
+    /// <summary>
+    ///     Represents the core data stored in a Tea Framework mod.
+    /// </summary>
+    public interface ITeaMod
+    {
+        /// <summary>
+        ///     The associated tModLoader <see cref="Mod"/>.
+        /// </summary>
+        Mod ModInstance { get; }
+    }
+}
--- TeaMod.cs
+++ TeaMod.cs
@@ -3,11 +3,32 @@
 // GNU General Public License Version 3, 29 June 2007
 #endregion
 
+using System;
+using System.Reflection;
 using Terraria.ModLoader;
 
 namespace TeaFramework
 {
-	public class TeaMod : Mod
+	/// <summary>
+	///		The <see cref="Mod"/> instance used by tModLoader for Tea Framework. Your mods will typically inherit this.
+	/// </summary>
+	/// <remarks>
+	///		If you do not want to inherit this class, you can implement functionality directly with <see cref="ITeaMod"/>.
+	/// </remarks>
+	public class TeaMod : Mod, ITeaMod
 	{
+		Mod ITeaMod.ModInstance => this;
+
+		/// <summary>
+		///		Executes a tasks only intended to be done by Tea Framework. Used as a workaround for a tModLoader issue (TML-2332).
+		/// </summary>
+		private bool ExecutePrivately(Action task)
+		{
+			if (!GetType().Assembly.FullName?.StartsWith("TeaFramework, ") ?? false)
+				return false;
+
+			task();
+			return true;
+		}
 	}
 }
