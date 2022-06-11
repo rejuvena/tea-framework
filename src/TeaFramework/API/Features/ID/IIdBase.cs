601c33b49d0bb3a2f0b75aa8b0f9b003a648e26d
Tomat <xxlennygamerxx@gmail.com>
2022-06-10 21:08:13 -0700

    [API] Add IdBase for XIDs

---
+++ src/TeaFramework/API/Features/ID/IIdBase.cs
@@ -0,0 +1,15 @@
+﻿using ReLogic.Reflection;
+
+namespace TeaFramework.API.Features.ID
+{
+    /// <summary>
+    ///     Serves as a consistent base for ID classes in a nearly-identical vein.
+    /// </summary>
+    /// <typeparam name="TIdBase">The class, should be itself.</typeparam>
+    /// <typeparam name="TType">The type each ID uses. Often an integer.</typeparam>
+    public interface IIdBase<TIdBase, TType>
+        where TIdBase : IIdBase<TIdBase, TType>
+    {
+        public static IdDictionary Search { get; } = IdDictionary.Create<TIdBase, TType>();
+    }
+}
