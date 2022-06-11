82ce016e234646ca76715fe1a7489620859df1a6
Tomat <xxlennygamerxx@gmail.com>
2022-06-10 21:11:47 -0700

    [Feature] Add BankID

---
+++ src/TeaFramework/Features/ID/BankID.cs
@@ -0,0 +1,14 @@
+﻿using TeaFramework.API.Features.ID;
+
+namespace TeaFramework.Features.ID
+{
+    // TODO: Figure out if (and how) we should specify "Count", since these are negatives.
+    public class BankID : IIdBase<BankID, int>
+    {
+        public const int None = -1;
+        public const int PiggyBank = -2;
+        public const int Safe = -3;
+        public const int DefendersForge = -4;
+        public const int VoidVault = -5;
+    }
+}
