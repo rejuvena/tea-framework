a8f921b27bfa8eed7c55f1663548f1f639d99f0f
Tomat <xxlennygamerxx@gmail.com>
2022-06-10 21:11:59 -0700

    [Feature] Add MountDrawTypeID

---
+++ src/TeaFramework/Features/ID/MountDrawTypeID.cs
@@ -0,0 +1,12 @@
+﻿using TeaFramework.API.Features.ID;
+
+namespace TeaFramework.Features.ID
+{
+    public class MountDrawTypeID : IIdBase<MountDrawTypeID, int>
+    {
+        public const int BackTexture = 0;
+        public const int BackTextureExtra = 1;
+        public const int FrontTexture = 2;
+        public const int FrontTextureExtra = 3;
+    }
+}
